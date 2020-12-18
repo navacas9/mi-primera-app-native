f77c42d7c853cf70dd03400565e14c90179ad5ad
ricardonava <rnavarrete@quaxar.com>
2020-12-17 22:40:32 -0600

    Cargando fonts usando expo-font

--- src/consts/index.js
+++ src/consts/index.js
@@ -3,3 +3,6 @@ export const LOGIN = 'Login';
 export const HOME = ' Home';
 export const RESULTS = 'Results';
 export const PROFILE = 'Profile';
+
+export const ROBOTO_FONT = 'Roboto';
+export const ROBOTO_MEDIUM_FONT = 'Roboto_medium';
--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // Se envuelven todas las rutas dentro del provider
 import { Provider } from 'react-redux';
+import * as Font from 'expo-font';
+import { Spinner } from 'native-base';
 
 import Routes from './routes';
 import Store from './redux/store'; // se importa desde store
+import { ROBOTO_FONT, ROBOTO_MEDIUM_FONT } from './consts';
 
 const store = Store(); // se usa la funcion de store
+const ROBOTO = require('../node_modules/native-base/Fonts/Roboto.ttf');  // Fuentes de native base
+const ROBOTO_MEDIUM = require('../node_modules/native-base/Fonts/Roboto_medium.ttf');
 
-export default () => (
-    // se pasa la store que se acaba de crear
-    <Provider store={store}> 
-        <Routes/>
-    </Provider>
-);
+export default () => {
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+    useEffect(() => {
+        loadFonts();
+    }, [fontsLoaded]);
+
+    const loadFonts = async () => {
+        await Font.loadAsync({
+            [ROBOTO_FONT]: ROBOTO,
+            [ROBOTO_MEDIUM_FONT]: ROBOTO_MEDIUM
+        });
+
+        setFontsLoaded(true);
+        
+    };
+
+    if (!fontsLoaded) {
+        return <Spinner color="red"></Spinner>
+    }
+    return (
+        // se pasa la store que se acaba de crear
+        <Provider store={store}> 
+            <Routes/>
+        </Provider>
+    );
+}
